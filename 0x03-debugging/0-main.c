ca6bb67306b0d0cf189e7979bf8d88727bf4a3a6
* <voluwadamilare2@gmail.comgit config --global user.name 23oluwadamilaregit config user.email voluwadamilare2@gmail.com>
2023-07-20 21:22:05 +0100

    0x03-debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,4 @@
-#include "main.h"
-
-/**
+#include "main.h"                                                                                   /**
 * main - Test function for positive or negative
 * Return: 0
 */
@@ -8,9 +6,8 @@
 int main(void)
 {
         int i;
-
-        i = 0;
+                                                          i = 0;
         positive_or_negative(i);
 
-        return (0);
-}
+        return (0);                               }
+
