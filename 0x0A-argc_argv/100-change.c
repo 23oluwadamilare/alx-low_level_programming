32e13a2e2e421d5d5093be9c32f4278e468c449d
* <voluwadamilare2@gmail.comgit config --global user.name 23oluwadamilaregit config user.email voluwadamilare2@gmail.com>
2023-08-05 03:43:15 +0100

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 
 /**
- * main - Prints the maximum number of coins to make change for an amount of money.
+ * main - Prints the maximum number of coins to
+ *        make change for an amount of money.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: If the number of arguements is not exactly one - 1. otherwise - 0.
+ * Return: If the number of arguements is not
+ *         exactly one - 1. Otherwise - 0.
  */
 int main(int argc, char *argv[])
 {
